7591477dce0913fc202b3312ddde35027308ffa2
Artsiom Trushko <artemchiik@gmail.com>
2022-05-14 14:43:28 +0300

    solved

--- src/main/java/com/epam/rd/autotasks/CarouselRun.java
+++ src/main/java/com/epam/rd/autotasks/CarouselRun.java
@@ -1,13 +1,29 @@
 package com.epam.rd.autotasks;
 
 public class CarouselRun {
+    protected final int[] arrayCopy = DecrementingCarousel.array.clone();
+    protected int position = 0;
 
     public int next() {
-       throw new UnsupportedOperationException();
+       //throw new UnsupportedOperationException();
+
+        if (isFinished())
+            return -1;
+        else {
+            while (arrayCopy[position %= arrayCopy.length] <= 0) {
+                position++;
+            }
+        }
+        return arrayCopy[position++]--;
     }
 
     public boolean isFinished() {
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
+
+        for (int el : arrayCopy)
+            if (el > 0)
+                return false;
+        return true;
     }
 
 }
--- src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
@@ -1,15 +1,39 @@
 package com.epam.rd.autotasks;
 
-public class DecrementingCarousel {
-    public DecrementingCarousel(int capacity) {
+import java.util.Arrays;
 
+public class DecrementingCarousel {
+    static int[] array;
+    int numberOfRuns = 0;
+
+    public DecrementingCarousel(int capacity) {
+        array = new int[capacity];
+        Arrays.fill(array, 0);
     }
 
     public boolean addElement(int element){
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
+        boolean flag = false;
+        if (numberOfRuns > 0){return flag;}
+        if (element > 0){
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] == 0){
+                    array[i] = element;
+                    flag = true;
+                    break;
+                }
+            }
+        }
+        return flag;
     }
 
     public CarouselRun run(){
-       throw new UnsupportedOperationException();
+       //throw new UnsupportedOperationException();
+
+        numberOfRuns++;
+        if (numberOfRuns > 1){
+            return null;
+        }
+        else {return new CarouselRun();}
     }
 }
